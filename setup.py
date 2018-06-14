50611bcece99da78290be1b27ca0014fbd621035
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-06-14 16:11:49 +0200

    Add required libraries

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     url='https://github.com/djc/abna',
     license='MIT',
     packages=setuptools.find_packages(),
+    install_requires=['cryptography', 'requests'],
     classifiers=(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
