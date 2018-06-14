ab77a58aa251d5c0ae1438bf681f72f446d6abfd
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-06-14 16:15:41 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name='abna',
-    version='0.1.0',
+    version='0.1.1',
     author='Dirkjan Ochtman',
     author_email='dirkjan@ochtman.nl',
     description='Automated retrieval of mutations from ABN Amro',
