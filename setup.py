cf18ebddb813db134954a8e5f038b361699f0ac6
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-06-14 16:01:33 +0200

    Add packaging infrastructure

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='abna',
+    version='0.1.0',
+    author='Dirkjan Ochtman',
+    author_email='dirkjan@ochtman.nl',
+    description='Automated retrieval of mutations from ABN Amro',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/djc/abna',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+)
